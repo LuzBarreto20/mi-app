6f2ee9679b8d056bdf7c611cb144cea74ae75ae6
luzbarreto20 <82717444+LuzBarreto20@users.noreply.github.com>
2022-01-27 14:47:00 -0300

    Mi aplicación

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Button extends Component {
+    state = {}
+    constructor(props) {
+        super(props)
+        console.log('constructor', props);
+    }
+
+    componentDidMount() {
+        console.log('componentDiMount');
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('component did update', prevProps, prevState);
+    }
+
+    componentWillUnmount() {
+        console.log('desmontando componente', this.props, this.state);
+    }
+
+    render() {
+        console.log('ejecutando metodo render de button');
+    return (
+        <button onClick={() => this.setState({props: 1})} >
+        Enviar
+        </button>
+    )
+
+    } 
 }
 
-export default App;
+class App extends Component {
+    state = {
+        valor: 3
+    }
+    render() {
+        console.log(this.state);
+        return (
+            <div>
+               <p>Hola mundo</p>  
+               {this.state.valor === 3
+               ? <Button chanchito='felizzz' /> 
+            : null }
+               <button onClick={() => this.setState({ valor: 2})}>
+                   Enviar en App
+               </button>
+            </div>
+        )
+    }
+}
+
+export default App
